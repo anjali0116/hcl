f6927b75c422340f7643d6e449cfc238d6b4e3bb
anjali0116 <anjalimadishetti11@gmail.com>
2021-05-28 13:21:42 +0530

    first commit

---
+++ src/firstproject/helloworld.java
@@ -0,0 +1,22 @@
+package firstproject;
+
+
+
+public class helloworld 
+{
+	enum juiceEnumVariable{SMALL, MEDIUM, LARGE};
+	juiceEnumVariable size;
+}
+
+ class FreshJuiceTest
+{
+	public static void main(String[] args[])
+	{
+		helloworld juice= new helloworld();
+		juice.size= helloworld.juiceEnumVariable.MEDIUM;
+		System.out.println("SIZE:"+juice.size);
+		
+	}
+}
+
+
---
+++ src/firstproject/xxx.java
@@ -0,0 +1,24 @@
+package firstproject;
+
+import java.util.ArrayList;
+
+public class xxx 
+{
+	int classNo;
+	int maxStrength;
+	ArrayList<helloworld> studentList;
+	/*public xxx(int classNo, int maxStrength, ArrayList<helloworld> studentList) {
+		super();
+		this.classNo = classNo;
+		this.maxStrength = maxStrength;
+		this.studentList = studentList;
+	}*/
+	public xxx(int classNo, int maxStrength, ArrayList<helloworld> studentList) {
+		super();
+		this.classNo = classNo;
+		this.maxStrength = maxStrength;
+		this.studentList = studentList;
+	}
+	
+
+}
